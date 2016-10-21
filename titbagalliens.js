7d0df12c9c9ab91b217d95baa1af920b1b00686a
Keyen331 <banayos.kay@gmail.com>
2016-10-21 09:15:35 -1000

    updating

---
+++ titbagalliens.js
@@ -0,0 +1,18 @@
+var greenAmnt = 9000;
+var redAmnt  = 500;
+var blueAmnt = 500;
+
+if (greenAmnt + redAmnt > 8000) {
+  console.log("It's christmas already?");
+}
+else if (greenAmnt > 5000) {
+  console.log("Ugh, vegetables....");
+}
+
+else if (blueAmnt + redAmnt <3000) {
+  console.log("We're all gonna die!!!!");
+}
+
+else if (greenAmnt + redAmnt + blueAmnt < 10000) {
+  console.log("call the avengers!!!!!")
+}
